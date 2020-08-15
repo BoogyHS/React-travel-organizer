07cfaaf7270477441e0b54a96b898fac66af45a6
Boogy83 <bozhidar.atanasov@abv.bg>
2020-08-15 12:47:19 +0300

    edit

--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './style.module.css'
 
+//context 
+import userContext from '../../Contexts/UserContext'
+
 function Profile() {
+    const [user, setUser] = useContext(userContext);
+    console.log(user)
+
     return (
-        <div className={styles.profile}>Profile</div>
+        <div className={styles.profile}>{JSON.stringify(user)}</div>
     )
 }
 
